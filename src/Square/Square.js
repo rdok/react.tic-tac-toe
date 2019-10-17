c04a59cd70179806ed91f98b600b291703a6dc2a
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-17 12:51:34 +0100

    Assert it sets the square value when clicked.

--- src/Square/Square.js
+++ src/Square/Square.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 export default class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { value: null }
+    }
     render() {
         return (
-            <button className="square" data-testid="squarealert" onClick={() => alert('clicked')}>
-            {this.props.value}
+            <button 
+                className="square" 
+                data-testid="squarealert" 
+                onClick={() => this.setState({value: 'X'})}
+            >
+            {this.state.value}
             </button>
         );
     }
--- src/Square/Square.test.js
+++ src/Square/Square.test.js
@@ -1,46 +1,9 @@
 import React from 'react';
 import Square from './Square';
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
 
-let container = null;
-beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-});
 
-afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-});
+it('default state value to null', () => {
+    let square = new Square()
 
-it('renders without crashing', () => {
-    act(() => { render(<Square />, container); });
-});
-
-it('renders without a square', () => {
-    const expected = `<button class="square" data-testid="squarealert"></button>`
-    act(() => { render(<Square />, container); });
-    expect(container.innerHTML).toBe(expected);
-});
-
-it('renders a square', () => {
-    const expected = `<button class="square" data-testid="squarealert">5</button>`
-    act(() => { render(<Square value={5} />, container); });
-    expect(container.innerHTML).toBe(expected);
-});
-
-it('alerts when square is clicked', () => {
-    window.alert = jest.fn();
-
-    act(() => { render(<Square />, container); });
-
-    const square = document.querySelector("[data-testid=squarealert]");
-
-    act(() => {
-        square.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-    });
-
-    expect(window.alert).toHaveBeenCalledWith('clicked');
-});
+    expect(square.state).toMatchObject({ value: null })
+})
