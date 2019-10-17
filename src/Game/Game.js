a44aa8be55a3f47079b6aa207f1562e1c35354e3
Rizart Dokollari <r.dokollari@gmail.com>
2019-10-17 18:15:20 +0100

    Assert Game sets the history state.

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -3,17 +3,26 @@ import './Game.css';
 import Board from './../Board/Board';
 
 export default class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }]
+        }
+    }
+
+    render() {
+        return (
+            <div className="game">
+            <div className="game-board">
+            <Board />
+            </div>
+            <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+            </div>
+        );
+    }
 }
--- src/Game/Game.test.js
+++ src/Game/Game.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game';
+import React from 'react'
+import Game from './Game'
+import Square from './../Square/Square'
+import { render, unmountComponentAtNode } from "react-dom"
+import { act } from "react-dom/test-utils"
+import { renderToString } from 'react-dom/server'
+
+
+let container = null
+beforeEach(() => {
+    container = document.createElement("div")
+    document.body.appendChild(container)
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container)
+    container.remove()
+    container = null
+})
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    act(() => { render(<Game />, container) })
+});
+
+it('sets the history state', () => {
+    let game = new Game 
+    const history = {history: [{ squares: Array(9).fill(null) }]}
+    expect(game.state).toMatchObject(history)
 });
